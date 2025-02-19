d552284485a52150430e2e1f505ddc4473cdc825
Naydiam <naydiam42@gmail.com>
2025-02-19 22:43:18 +0000

    adding the sigint

--- msh.c
+++ msh.c
@@ -43,6 +43,11 @@
 
 #define MAX_NUM_ARGUMENTS 11     // Mav shell currently only supports one argument
 
+static void handle_signal (int sig )
+{
+  printf ("Caught signal %d\n", sig );
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -169,7 +174,32 @@ int main(int argc, char *argv[])
         }
         
       }
+
+      struct sigaction act;
+ 
+  
+      memset (&act, '\0', sizeof(act));
+ 
+  /*
+    Set the handler to use the function handle_signal()
+  */ 
+      act.sa_handler = &handle_signal;
+ 
+  /* 
+    Install the handler and check the return value.
+  */ 
+      if (sigaction(SIGINT , &act, NULL) < 0) 
+      {
+        perror ("sigaction: ");
+        return 1;
+      }
+
+      while (1) 
+      {
+        sleep (1);
+      }
     }
+
     // Cleanup allocated memory
     for( int i = 0; i < MAX_NUM_ARGUMENTS; i++ )
     {
